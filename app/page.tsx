ff0e99f2878e249b4f5cc6e8792fa35fed9a513e
George Micu <george.micu00@gmail.com>
2024-05-18 17:24:49 +0100

    Added like button and rating label to home cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import HomeCard from "@/components/HomeCard";
 import exampleImage from "../images/Example1.jpg"
 
@@ -6,36 +8,41 @@ export default function Home() {
         {
             title: "Example1",
             description: "Description1",
-            image: exampleImage
+            image: exampleImage,
+            rating: 5
         },
         {
             title: "Example2",
             description: "Description2",
-            image: exampleImage
+            image: exampleImage,
+            rating: 4.5
         },
         {
             title: "Example3",
             description: "Description3",
-            image: exampleImage
+            image: exampleImage,
+            rating: 3.5
         },
         {
             title: "Example4",
             description: "Description4",
-            image: exampleImage
+            image: exampleImage,
+            rating: 4.9
         },
         {
             title: "Example5",
             description: "Description5",
-            image: exampleImage
+            image: exampleImage,
+            rating: 5
         }
     ]
 
   return (
       <div className="flex-1">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-5">
               {
                   cards.map((card, index) => (
-                      <HomeCard key={index} title={card.title} description={card.description} image={card.image} />
+                      <HomeCard key={index} title={card.title} description={card.description} image={card.image} rating={card.rating} />
                   ))
               }
           </div>
--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,16 +1,33 @@
 import Image from 'next/image'
+import { FaHeart, FaRegHeart, FaStar } from 'react-icons/fa';
+import {useState} from "react";
 
 interface HomeCardProps {
     title: string;
     description: string;
     image: any;
+    rating: number;
 }
 
-export default function HomeCard({ title, description, image }: HomeCardProps) {
+export default function HomeCard({ title, description, image, rating }: HomeCardProps) {
+    const [liked, setLiked] = useState(false);
+
     return (
-        <div className="border rounded-lg overflow-hidden shadow-lg">
-            <Image src={image} alt="Example Image"/>
-            <div className="flex flex-col justify-center p-2 bg-gray-100 flex-grow">
+        <div className="w-full border rounded-lg overflow-hidden relative">
+            <div className="relative">
+                <Image src={image} alt="Example Image"/>
+                <div
+                    className="absolute top-5 -left-1 bg-white text-gray-700 p-2 flex items-center h-4 rounded border">
+                    <FaStar className="mr-1"/>
+                    <span className="font-bold text-sm">{rating}</span>
+                </div>
+                <button
+                    onClick={() => setLiked(!liked)}
+                    className="absolute top-4 right-3 bg-white text-green-800 rounded-full p-2 shadow-md focus:outline-none">
+                    {liked ? <FaHeart/> : <FaRegHeart/>}
+                </button>
+            </div>
+            <div className="flex flex-col justify-center p-2 flex-grow">
                 <h3 className="text-lg font-bold mb-1">{title}</h3>
                 <p>{description}</p>
             </div>
