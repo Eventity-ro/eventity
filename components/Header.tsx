009ee8a94f7293f090373130a62c40ec57c8bdea
George Micu <george.micu00@gmail.com>
2024-11-28 18:27:37 +0000

    Added new logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     return (
         <div className="bg-white">
             <div className="flex flex-col lg:flex-row justify-between items-center gap-4 lg:gap-[20%] pt-4 pb-2 px-4 lg:px-12">
-                <Image src={logo} alt="logo" width={50} height={50} />
+                <Image src={logo} alt="logo" width={100} height={100} />
                 <SearchBar/>
                 <div className="flex items-center gap-2">
                     <AppButton title="Devino client"/>
