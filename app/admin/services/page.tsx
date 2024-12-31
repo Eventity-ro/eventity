fa8250d85ea4ef192b6f694cda86ab408e1042b7
George Micu <george.micu00@gmail.com>
2024-12-31 10:29:38 +0000

    Fixed index error when building a map

--- app/admin/services/page.tsx
+++ app/admin/services/page.tsx
@@ -43,8 +43,8 @@ export default function Services() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m y-5">
                 {
                     cards.map((card, index) => (
-                        <div>
-                            <HomeCard key={index} title={card.title} location={card.location}
+                        <div key={index}>
+                            <HomeCard title={card.title} location={card.location}
                                       startingPrice={card.startingPrice} imageList={card.imageList} rating={card.rating}
                                       capacity={card?.capacity}/>
                             <Link className="bg-[#5C8171] text-white px-6 py-2 rounded mb-5" href={"/admin/services/new"}>
